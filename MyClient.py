2918d5a37b098ebd16de295beec1359d2552b6fe
Jonathan Isely <jmisely19@gmail.com>
2024-12-01 16:58:05 -0500

    Got working test done

--- MyClient.py
+++ MyClient.py
@@ -2,9 +2,15 @@ import discord
 
 class MyClient(discord.Client):
     
-    def init():
+    def init(self):
         pass
     
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+
+        if message.content.startswith('$$test'):
+            await message.channel.send('Hello!')
     
     async def on_ready(self):
         print(f"Logged on as {self.user}")
--- main.py
+++ main.py
@@ -3,18 +3,17 @@ import os
 from dotenv import load_dotenv
 from MyClient import MyClient
 
+
+
 def main():
     load_dotenv()
     TOKEN_KEY = os.getenv('TOKEN_KEY')
-    
-    
     intents = discord.Intents.default()
     intents.message_content = True
     intents.presences = True
     intents.members = True
     client = MyClient(intents=intents)
     client.run(TOKEN_KEY)
-
-
+    
 if __name__=="__main__":
     main()
