770fe4d21de48a17d530617d40e7a5ff9c380fb7
louis <louisjfortunato@gmail.com>
2024-12-01 19:39:23 -0500

    randomization

--- MyClient.py
+++ MyClient.py
@@ -1,5 +1,6 @@
 import discord
 from SSUser import SSUser
+from numpy import random
 
 class MyClient(discord.Client):
     
@@ -16,22 +17,36 @@ class MyClient(discord.Client):
     async def randomize_secret_santa_process(self, message):
         self.user_list = []
         self.set_channel_id(message.channel.id)
-        self.generate_SSusers()
-        # self.assign_secret_santas()
+        self.assign_secret_santas(self.generate_SSusers())
 
-        
     def generate_SSusers(self):
         channel = self.get_channel(self.get_channel_id())
         members = channel.members
         for member in members:
-            self.user_list.append(SSUser(member))
+            if member.id != 1312872037479350293:
+                self.user_list.append(SSUser(member))
+        return self.user_list
 
-    def assign_secret_santas(self):
-        users = self.generate_user_list()
+    def choose_candidate(self, member_list):
+        return member_list[random.randint(0,len(member_list))]
 
+    def assign_secret_santas(self,users):
+        unassigned_participants = []
+        for user in users:
+            unassigned_participants.append(user)
+        for user in users:
+            candidate = user
+            while candidate == user:
+                candidate = self.choose_candidate(unassigned_participants)
+            unassigned_participants.remove(candidate)
+            user.set_ssid(candidate._id)
+            user.set_ssname(candidate._name)
 
+            print(user._name + ', ' + user._ssname)
+        
+    
     def get_channel_id(self):
         return self.channel_id
     
     def set_channel_id(self, id):
-        self.channel_id = id
+        self.channel_id = id     
--- SSUser.py
+++ SSUser.py
@@ -1,10 +1,12 @@
 class SSUser():
     
     def __init__(self, member):
-        print(member.id)
+        
         self._name = member.name  # Private attribute
         self._id = member.id # Private attribute
         self._ssid = "" # Private attribute
+        self._ssname = "" # Private attribute
+        # print(self._name + ', ' + str(self._id))
 
     def get_name(self):
         return self._name
@@ -20,3 +22,9 @@ class SSUser():
 
     def set_ssid(self, ssid):
         self._ssid = ssid
+
+    def get_ssname(self):
+        return self._ssname
+
+    def set_ssname(self, ssname):
+        self._ssname = ssname
